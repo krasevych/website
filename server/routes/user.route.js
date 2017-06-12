7ee09ff26b9082982fa86b29fc5bd85a3f907cc3
Roman <krasevych@animatron.com>
2017-06-12 20:38:38 +0300

    user router

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,20 +4,25 @@ import User from '../models/user.model';
 
 const router = express.Router();
 
-router.route('/login')
+router
+  .route('/login')
   .post((req, res, next) => {
-
+  console.log(333, req.body)
+    User.find({ email: req.body.email }).then((user) => {
+      console.log(111, user.toAuthJSON());
+      res.json(user.toAuthJSON());
+    });
   })
   .post((req, res, next) => {
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password,
-  });
+    const user = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-  user
-    .save()
-    .then(savedUser => res.json(savedUser.toAuthJSON()))
-    .catch(e => next(e));
-});
+    user
+      .save()
+      .then(savedUser => res.json(savedUser.toAuthJSON()))
+      .catch(e => next(e));
+  });
 
 export default router;
